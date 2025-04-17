74838b5bc30067e8b351b6bedabc6df3b19b07b6
liav <161258307+PROL3@users.noreply.github.com>
2025-04-17 15:52:48 +0300

    Delete client directory

--- client/src/components/Layout/Header/Header.tsx
+++
@@ -1,9 +0,0 @@
-import DarkModeToggle from '../../DarkModeToggle';
-
-export default function Header() {
-  return (
-    <header className='bg-pink-200 shadow-sm dark:bg-gray-800 dark:shadow-dark-sm pt-5'>
-      <DarkModeToggle  size={15} />
-    </header>
-  );
-}
